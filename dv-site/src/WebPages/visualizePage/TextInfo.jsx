97ba67b439ed5a96e4ff24e4ac598c9d31f43826
Anna Ho <aymieoh@gmail.com>
2023-12-12 12:57:02 -0500

    DEGList expand
    DEG List Expand

--- dv-site/src/WebPages/visualizePage/TextInfo.jsx
+++ dv-site/src/WebPages/visualizePage/TextInfo.jsx
@@ -1,12 +1,13 @@
 import "./textinfo.css";
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function VisualizeInformation({ onClick }) {
   return (
     <div className="instructions-container" onClick={onClick}>
-      <div className="instructions-header">
-        <p>Visualize DEGlist Datasets</p>
-      </div>
+      <Link className="instructions-header">
+        <a>Visualize DEGlist Datasets</a>
+      </Link>
     </div>
   );
 }
