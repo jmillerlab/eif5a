4c29799d3cc2d80a472948d4bbc25eb3ac4a5b12
Anna Ho <aymieoh@gmail.com>
2024-01-03 08:30:54 -0500

    test

--- dv-site/src/HomePage.jsx
+++ dv-site/src/HomePage.jsx
@@ -2,18 +2,10 @@ import "./App.css";
 import HomeHero from "./homeComponents/HomeHero";
 import HomeDescription from "./homeComponents/HomeDescription";
 import HomeLearnMore from "./homeComponents/HomeLearnMore";
-import Heatmap from "./graphs/Heatmap";
-import HomeFooter from "./homeComponents/HomeFooter";
 
 function HomePage() {
   return (
     <>
-      {/* 
-    <div className="image">
-       
-      </div>
-    */}
-
       <HomeHero />
       <HomeDescription />
       <HomeLearnMore />
