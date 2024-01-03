114b98567a799f04b2731f6276b6d94314b1ab4d
Anna Ho <aymieoh@gmail.com>
2024-01-03 01:11:41 -0500

    test

--- dv-site/src/WebPages/About.jsx
+++ dv-site/src/WebPages/About.jsx
@@ -1,3 +1,4 @@
+import "./about.css";
 // import Venn from "./visualizePage/VennDiagrams";
 export default function About() {
   return (
@@ -5,6 +6,7 @@ export default function About() {
       <div className="about-inner-container">
         <div className="about-header">
           <h1>Project Overview Page</h1>
+          <p>Under Construction</p>
         </div>
       </div>
     </div>
