6e253d4b67a406f6b4b62d8320be0ed7cc699052
Anna Ho <aymieoh@gmail.com>
2024-01-20 12:59:22 -0500

    changes
    changes

--- dv-site/src/App.jsx
+++ dv-site/src/App.jsx
@@ -6,8 +6,6 @@ import Publications from "./WebPages/Publications";
 import Visualize from "./WebPages/Visualize";
 import HomeFooter from "./homeComponents/HomeFooter";
 import { Route, Routes, useLocation } from "react-router-dom";
-import { motion as m } from "framer-motion";
-import { AnimatePresence } from "framer-motion";
 import ContactSection from "./ContactSection";
 
 function App() {
@@ -19,40 +17,10 @@ function App() {
       <div className="">
         <Navbar />
         <Routes>
-          <Route
-            path="/"
-            element={
-              <AnimatePresence initial={false}>
-                <HomePage key={location} />
-              </AnimatePresence>
-            }
-          />
-          <Route
-            path="/about"
-            element={
-              // <div className="image">
-              <AnimatePresence initial={false}>
-                <About />
-              </AnimatePresence>
-              // </div>
-            }
-          />
-          <Route
-            path="/studies"
-            element={
-              <AnimatePresence initial={false}>
-                <Publications />
-              </AnimatePresence>
-            }
-          />
-          <Route
-            path="/visualize"
-            element={
-              <AnimatePresence initial={false}>
-                <Visualize key={location} />
-              </AnimatePresence>
-            }
-          />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/studies" element={<Publications />} />
+          <Route path="/visualize" element={<Visualize />} />
         </Routes>
       </div>
       <ContactSection />
--- dv-site/src/graphs/ScatterPlot.py
+++ dv-site/src/graphs/ScatterPlot.py
@@ -1,38 +1,3 @@
-# import pandas as pd
-# import plotly.express as px
-# import plotly.io as pio
-
-# def create_scatterplot(csv_path, output_html_path):
-#     data = pd.read_csv(csv_path)
-#     scatterplot = px.scatter(
-#         data_frame=data,
-#         x="log2FoldChange",
-#         hover_data=["gene_name", "gene_id"],
-#         y="-log10(padj)",
-#         size_max=13,
-#         color="-log10(padj)",
-#         color_continuous_scale=px.colors.sequential.Bluered,      # set marker colors. When color colum is numeric data
-
-#     )
-
-#     scatterplot.update_layout(
-#         autosize=True,
-#         margin=dict(l=20, r=20, t=20, b=20),
-#         plot_bgcolor='white',
-
-#     )
-
-#     scatterplot.update_xaxes(showgrid=True, gridwidth=0.5, gridcolor='lightgray', zeroline=True, zerolinecolor='lightgray')
-#     scatterplot.update_yaxes(showgrid=True, gridwidth=0.5, gridcolor='lightgray')
-
-#     # Set explicit axis ranges to include zero
-#     scatterplot.update_yaxes(range=[data['-log10(padj)'].min(), data['-log10(padj)'].max()])
-
-#     pio.write_html(scatterplot, file=output_html_path, full_html=False, include_plotlyjs='cdn')
-
-# if __name__ == "__main__":
-#     create_scatterplot('DHS_DOHHvsWT_EC\DHS_DOHHvsWT_EC.all.cancer.csv', 'DHS_DOHHvsWT_EC\DHS_DOHHvsWT_EC.all.cancer.html')
-#     create_scatterplot('DHS_DOHHvsWT_EC\DHS_DOHHvsWT_EC.DEG.all.csv', 'DHS_DOHHvsWT_EC\DHS_DOHHvsWT_EC.DEG.all.html')
 
 import pandas as pd
 import plotly.express as px
